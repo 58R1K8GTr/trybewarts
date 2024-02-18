fa2f6c2c9f8683abd245a08faf642a3c4ca965c5
b166erobot <bernard0freitas@outlook.com>
2024-02-17 21:52:03 -0300

    Requisito 14 satisfeito

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const buttonLogin = document.querySelector('#button-login');
 const inputEmail = document.querySelector('#input-email-login');
 const inputPassword = document.querySelector('#input-password-login');
+const containerRate = document.querySelector('#container-rate');
 
+// code.
 function login(event) {
   event.preventDefault();
   (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456')
@@ -11,3 +13,14 @@ function login(event) {
 }
 
 buttonLogin.addEventListener('click', login);
+
+for (let indexInput = 1; indexInput < 11; indexInput += 1) {
+  const label = document.createElement('label');
+  const inputCheckbox = document.createElement('input');
+  inputCheckbox.type = 'radio';
+  inputCheckbox.name = 'rate';
+  inputCheckbox.value = indexInput;
+  label.innerText = indexInput;
+  label.appendChild(inputCheckbox);
+  containerRate.appendChild(label);
+}
